3634b4fba769b56093ac6fb21fcfe4c5ded48bc1
shivam <shivamkapar007@gmail.com>
2024-05-09 17:04:36 +0530

    server

--- js/business.js
+++ js/business.js
@@ -439,9 +439,9 @@ async function postData(url = "", data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "no-cors", // no-cors, *cors, same-origin
+    // mode: "no-cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "include", // include, *same-origin, omit
+    // credentials: "include", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
